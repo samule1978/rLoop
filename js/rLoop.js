98e1b679d29b252ffec367e5a5bfbfdd876c2c81
Sam G <samule.ghurry@coxauto.co.uk>
2018-01-18 13:07:47 +0000

    Added rotate script

--- js/rLoop.js
+++ js/rLoop.js
@@ -4,4 +4,36 @@ $(document).ready(function () {
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
         menu: '#menu',*/
     });
+
+    var delay = 100;
+    var beta = 0;
+
+    if (window.DeviceMotionEvent === undefined) {
+    } else {
+        window.ondevicemotion = function(event) {
+            ax = Math.round(Math.abs(event.accelerationIncludingGravity.x * 1));
+            ay = Math.round(Math.abs(event.accelerationIncludingGravity.y * 1));
+            az = Math.round(Math.abs(event.accelerationIncludingGravity.z * 1));
+            ai = Math.round(event.interval * 100) / 100;
+            rR = event.rotationRate;
+            if (rR != null) {
+                arAlpha = Math.round(rR.alpha);
+                arBeta = Math.round(rR.beta);
+                arGamma = Math.round(rR.gamma);
+            }
+        }
+
+        window.ondeviceorientation = function(event) {
+            alpha = Math.round(event.alpha);
+            beta = Math.round(event.beta);
+            gamma = Math.round(event.gamma);
+        }
+
+        function d2h(d) {return d.toString(16);}
+        function h2d(h) {return parseInt(h,16);}
+
+        setInterval(function() {
+            document.getElementById("betalabel").innerHTML = "Beta: " + beta;
+        }, delay);
+    }
 });
