c13683f24b93c80c39f21006b59edee9a0f2173f
Sam G <samule.ghurry@coxauto.co.uk>
2018-01-19 19:48:02 +0000

    ...

--- js/rLoop.js
+++ js/rLoop.js
@@ -75,9 +75,9 @@ $.fn.showDebugInfo = function(show) {
             document.getElementById("gammalabel").innerHTML = "Gamma: " + gamma;
 
             if($(this).portrait()) {
-                document.getElementById("orientationlabel").innerHTML = "Orientation: Portrait";
+                document.getElementById("orientationlabel").innerHTML = "Orientationsss: Portrait";
             } else {
-                document.getElementById("orientationlabel").innerHTML = "Orientation: Landscape";
+                document.getElementById("orientationlabel").innerHTML = "Orientationsss: Landscape";
             }
 
         }, delay);
