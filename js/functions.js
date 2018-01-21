97d3e7e1d7c25d2313068554a76a296a23dd4edf
Sam G <samule.ghurry@coxauto.co.uk>
2018-01-21 15:12:56 +0000

    ...

--- js/functions.js
+++ js/functions.js
@@ -39,10 +39,10 @@ $.fn.gyro = function() {
         window.ondeviceorientation = function(event) {
 
             //degrees = Math.round(event.alpha);
-            degrees = -(Math.round(event.gamma));
+            degrees = Math.round(event.gamma);
 
             if($(this).portrait()) {
-                $("#spinLogo").rotate(degrees);
+                $("#spinLogo").rotate(-degrees);
                 $("#spiniPhoneX").rotate(degrees);
             } else {
                 $("#spinLogo").rotate(0);
