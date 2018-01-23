5bfd70fa53d722bb066a185466715372feee9c5e
Sam G <samule.ghurry@coxauto.co.uk>
2018-01-23 17:06:19 +0000

    ...

--- js/functions.js
+++ js/functions.js
@@ -63,7 +63,6 @@ $.fn.animateOnOrientationChange = function() {
         $("#spiniPhoneX").removeAttr('style');
         $("#spinLogo").rotate(0);
         $("#spiniPhoneX").rotate(0);
-        alert('resize');
 
         if($(this).portrait()) {
         } else {
