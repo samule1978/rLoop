91a31f9b1b9cdfafd52f2b33d4914a46a1b256dd
Sam G <samule.ghurry@coxauto.co.uk>
2018-01-26 22:36:29 +0000

    ...

--- js/rLoop.js
+++ js/rLoop.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
 
         // Because we don't want to have the device upside down
         // We constrain the x value to the range [-90,90]
-        //if (x >  90) { x =  90};
-        //if (x < -90) { x = -90};
+        if (x >  90) { x =  90};
+        if (x < -90) { x = -90};
 
         // To make computation easier we shift the range of
         // x and y to [0,180]
