a29e56e4ecef427042f10a0597ce1c756eb7caf1
Sam G <samule.ghurry@coxauto.co.uk>
2018-01-26 22:42:06 +0000

    ...

--- js/rLoop.js
+++ js/rLoop.js
@@ -5,28 +5,36 @@
 
 $(document).ready(function () {
     window.ondeviceorientation = function(event) {
-        var x = event.beta;  // In degree in the range [-180,180]
-        var y = event.gamma; // In degree in the range [-90,90]
+        var x = Math.round(event.beta);  // In degree in the range [-180,180]
+        var y = Math.round(event.gamma); // In degree in the range [-90,90]
+        var left, right;
 
-        // Because we don't want to have the device upside down
-        // We constrain the x value to the range [-90,90]
-        if (x >  90) { x =  90};
-        if (x < -90) { x = -90};
+        if($(this).portrait()) {
+            left = 50 - y;
+            right = 50 + y;
 
-        // To make computation easier we shift the range of
-        // x and y to [0,180]
-        //x += 90;
-        //y += 90;
+            $(".containerRLoopIcon").rotate(y + "deg");
+            $(".loader-wrap-top").clipPathPolygonTop(y, left, right);
+            $(".loader-wrap-bottom").clipPathPolygonBottom(y, left, right);
 
-        x = Math.round(x);
-        y = Math.round(y);
+        } else {
+            // Because we don't want to have the device upside down
+            // We constrain the x value to the range [-90,90]
+            //if (x >  90) { x =  90};
+            //if (x < -90) { x = -90};
 
-        var left = 50 - y;
-        var right = 50 + y;
+            // To make computation easier we shift the range of
+            // x and y to [0,180]
+            //x += 90;
+            //y += 90;
 
-        $(".containerRLoopIcon").rotate(y + "deg");
-        $(".loader-wrap-top").clipPathPolygonTop(y, left, right);
-        $(".loader-wrap-bottom").clipPathPolygonBottom(y, left, right);
+            left = 50 - x;
+            right = 50 + x;
+
+            $(".containerRLoopIcon").rotate(x + "deg");
+            $(".loader-wrap-top").clipPathPolygonTop(x, left, right);
+            $(".loader-wrap-bottom").clipPathPolygonBottom(x, left, right);
+        }
 
         var debug = document.querySelector('.debug');
         debug.innerHTML  = "beta : " + x + "<br />";
