a26f23a4418cc229394129a6d3efca260cfc4016
samule1978 <sghurry@hotmail.com>
2018-01-27 03:23:28 +0000

    Update rLoop.js

--- js/rLoop.js
+++ js/rLoop.js
@@ -17,8 +17,8 @@ $(document).ready(function () {
             degrees = -y;
 		
             $(".containerRLoopIcon").rotate(degrees + "deg");
-            $(".loader-wrap-top").clipPathPolygonTop(y, 49.85 + y, 49.85 - y);
-            $(".loader-wrap-bottom").clipPathPolygonBottom(y, 50.15 + y, 50.15 - y);
+            $(".loader-wrap-top").clipPathPolygonTop(y, 69.85 + y, 69.85 - y);
+            $(".loader-wrap-bottom").clipPathPolygonBottom(y, 30.15 + y, 30.15 - y);
 
         } else {
             // Because we don't want to have the device upside down
@@ -34,8 +34,8 @@ $(document).ready(function () {
             degrees = x;
 
             $(".containerRLoopIcon").rotate(degrees + "deg");
-            $(".loader-wrap-top").clipPathPolygonTop(x, 49.85 - x, 49.85 + x);
-            $(".loader-wrap-bottom").clipPathPolygonBottom(x, 50.15 - x, 50.15 + x);
+            $(".loader-wrap-top").clipPathPolygonTop(x, 69.85 - x, 69.85 + x);
+            $(".loader-wrap-bottom").clipPathPolygonBottom(x, 30.15 - x, 30.15 + x);
         }
 
         var debug = document.querySelector('.debug');
