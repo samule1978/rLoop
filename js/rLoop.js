530d534d7416345e806017c64b6915c2bf9f3cca
Sam G <samule.ghurry@coxauto.co.uk>
2018-02-10 18:16:31 +0000

    ...

--- js/rLoop.js
+++ js/rLoop.js
@@ -3,26 +3,24 @@
  * Created by samuleghurry on 20/01/2018.
  */
 
-// Set mobile flag.
-isMobile = $(this).util_isMobile();
-
-// Set device type.
-var device = (isMobile) ? "mobile" : "desktop";
-$("html").addClass(device);
-
-// Add twinkling stars to body.
-$("body").util_showStars(true);
-
-// Hide main content.
-$("#main").hide();
-
-// Show loading animation.
-$(this).showLoadingAnimation();
-
-
 $(document).ready(function () {
     $(this).initialise();
 });
 
 $.fn.initialise = function() {
+    // Set mobile flag.
+    isMobile = $(this).util_isMobile();
+
+    // Set device type.
+    var device = (isMobile) ? "mobile" : "desktop";
+    $("html").addClass(device);
+
+    // Add twinkling stars to body.
+    $("body").util_showStars(true);
+
+    // Hide main content.
+    $("#main").hide();
+
+    // Show loading animation.
+    $(this).showLoadingAnimation();
 };
