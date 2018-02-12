df8a67c70810ef49baac15ea15b138fb8a28cbb0
Sam G <samule.ghurry@coxauto.co.uk>
2018-02-12 13:59:52 +0000

    ...

--- js/utilities.js
+++ js/utilities.js
@@ -118,8 +118,8 @@ $.fn.util_amendContentBasedOnOrientation = function() {
 };
 
 $.fn.util_amendContentBasedOnOrientationChange = function() {
-    alert($("orientation.portrait").is(":visible") ? "RR-Portrait" : "RR-Landscape");
+    alert($(this).util_portrait() ? "RR-Portrait" : "RR-Landscape");
     window.onresize = function (event) {
-        alert($("orientation.portrait").is(":visible") ? "RR-Portrait" : "RR-Landscape");
+        alert($(this).util_portrait() ? "RR-Portrait" : "RR-Landscape");
     };
 };
