410e1c21f160f8b4ca696b8ef5fc53ea70d4e004
Sam G <samule.ghurry@coxauto.co.uk>
2018-02-20 13:50:47 +0000

    ...

--- js/functions.js
+++ js/functions.js
@@ -50,11 +50,13 @@ $.fn.anim_Start_SceneOne = function() {
             .to(headlineWrapTop, 2, {top:100})
             .to(rLoopMenuBorder, 2, {width:"100%"}, '-=2')
             .to(bgPod, 3, {opacity:1}, '-=2')
-            .to(preLoadOuter, 5, {background:"rgba(0, 0, 0, 0.5)"}, '-=3');
+            .to(preLoadOuter, 5, {background:"rgba(0, 0, 0, 0.3)"}, '-=3');
 
 };
 $.fn.anim_Finish_SceneOne = function() {
     $("#" + _idRLoopContent).util_disableScrollFullPage(false);
+
+    $(this).anim_Start_SceneTwo();
 };
 $.fn.resize_SceneOne = function() {
     function set_css(elem_id, style) {
@@ -112,6 +114,92 @@ $.fn.resize_SceneOne = function() {
         backgroundPosition: bg_x + ' ' + bg_y + ', 0 ' + bg_y + ', ' + bg_x + ' 0%',
         backgroundRepeat: 'no-repeat, no-repeat, no-repeat',
     });
+};
+
+$.fn.anim_Start_SceneTwo = function() {
+    var ringOne = new addCoinDots({
+        parent:document.getElementById('sceneTwo').getElementsByClassName('section-graphics')[0],
+        radius:100,
+        dotSize:7,
+        dotCount:20,
+        colors:["#00fcff","#60fdff","#93fdff","#d3feff"], //have as many or as few colors as you want.
+        animationOffset: 1.8, //jump 1.8 seconds into the animation for a more active part of the spinning initially (just looks a bit better in my opinion)
+    });
+
+    ringOne.active(true);
+
+    //this is the whole preloader class/function
+    function addCoinDots(options) {
+        options = options || {};
+        var parent = options.parent || document.body,
+            element = this.element = document.createElement("div"),
+            radius = options.radius || 42,
+            dotSize = options.dotSize || 15,
+            animationOffset = options.animationOffset || 1.8, //jumps to a more active part of the animation initially (just looks cooler especially when the preloader isn't displayed for very long)
+            createDot = function(rotation) {
+                var dot = document.createElement("div");
+                element.appendChild(dot);
+                TweenLite.set(dot, {width:dotSize, height:dotSize, transformOrigin:(-radius + "px 0px"), x: radius, backgroundColor:colors[colors.length-1], borderRadius:"50%", force3D:true, position:"absolute", rotation:rotation});
+                dot.className = options.dotClass || "preloader-dot";
+                return dot;
+            },
+            i = options.dotCount || 10,
+            rotationIncrement = 360 / i,
+            colors = options.colors || ["#61AC27","black"],
+            animation = new TimelineLite({paused:true}),
+            dots = [],
+            isActive = false,
+            box = document.createElement("div"),
+            tl, dot, closingAnimation, j;
+        colors.push(colors.shift());
+
+        parent.appendChild(element);
+
+        TweenLite.set(element, {position:"relative", top:"50%", left:"50%", perspective:600, overflow:"visible", zIndex:2000});
+        animation.from(box, 0.1, {opacity:0, scale:0.1, ease:Power1.easeOut}, animationOffset);
+        while (--i > -1) {
+            dot = createDot(i * rotationIncrement);
+            dots.unshift(dot);
+            animation.from(dot, 0.1, {scale:0.01, opacity:0, ease:Power1.easeOut}, animationOffset);
+            //tuck the repeating parts of the animation into a nested TimelineMax (the intro shouldn't be repeated)
+            tl = new TimelineMax({repeat:-1, repeatDelay:0.25});
+            for (j = 0; j < colors.length; j++) {
+                tl.to(dot, 2.5, {rotation:"-=360", ease:Power2.easeInOut}, j * 2.9)
+                    .to(dot, 1.2, {skewX:"+=360", backgroundColor:colors[j], ease:Power2.easeInOut}, 1.6 + 2.9 * j);
+            }
+            //stagger its placement into the master timeline
+            animation.add(tl, i * 0.07);
+        }
+        if (TweenLite.render) {
+            TweenLite.render(); //trigger the from() tweens' lazy-rendering (otherwise it'd take one tick to render everything in the beginning state, thus things may flash on the screen for a moment initially). There are other ways around this, but TweenLite.render() is probably the simplest in this case.
+        }
+
+        //call preloader.active(true) to open the preloader, preloader.active(false) to close it, or preloader.active() to get the current state.
+        this.active = function(show) {
+            if (!arguments.length) {
+                return isActive;
+            }
+            if (isActive != show) {
+                isActive = show;
+                if (closingAnimation) {
+                    closingAnimation.kill(); //in case the preloader is made active/inactive/active/inactive really fast and there's still a closing animation running, kill it.
+                }
+                if (isActive) {
+                    element.style.visibility = "visible";
+                    TweenLite.set([element, box], {rotation:0});
+                    animation.play(animationOffset);
+                } else {
+                    closingAnimation = new TimelineLite();
+                    if (animation.time() < animationOffset + 0.3) {
+                        animation.pause();
+                        closingAnimation.to(element, 1, {rotation:-360, ease:Power1.easeInOut}).to(box, 1, {rotation:360, ease:Power1.easeInOut}, 0);
+                    }
+                    closingAnimation.staggerTo(dots, 0.3, {scale:0.01, opacity:0, ease:Power1.easeIn, overwrite:false}, 0.05, 0).to(box, 0.4, {opacity:0, scale:0.2, ease:Power2.easeIn, overwrite:false}, 0).call(function() { animation.pause(); closingAnimation = null; }).set(element, {visibility:"hidden"});
+                }
+            }
+            return this;
+        };
+    }
 };
 
 $.fn.resize_Scenes = function() {
