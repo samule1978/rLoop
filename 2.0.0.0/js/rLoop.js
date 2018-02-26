b814d62debb526e3df3557d389cbbb551248a02c
Sam G <samule.ghurry@coxauto.co.uk>
2018-02-26 16:49:27 +0000

    ...

---
+++ 2.0.0.0/js/rLoop.js
@@ -0,0 +1,28 @@
+/**
+ * **** BISMILLAH-HIRRAHMAH-NIRRAHEEM ***
+ * Created by samuleghurry on 20/01/2018.
+ */
+
+$(document).ready(function () {
+    $(this).initialise();
+});
+
+$.fn.initialise = function() {
+    incognito = false;
+    debugMobileLandscapeOnDesktop = false;
+
+    // Set mobile flag.
+    isMobile = $(this).util_isMobile();
+
+    // Set device type.
+    $(this).util_setDeviceType(isMobile);
+
+    // Add twinkling stars to body.
+    $("body").util_showStars(true);
+
+    // Initialise Content.
+    $("body").util_initialiseContent();
+
+    // Start animation.
+    $(this).anim_Start_SceneOne();
+};
